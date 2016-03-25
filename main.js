cab9a06c39fe119285fe8dbb8620e74b2cce509c
Will Cook <wellsee83@gmail.com>
2016-03-25 13:53:39 -0500

    Updated Post Settings

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
+//main page~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var collectionUrl = 'http://small-tiyfe.herokuapp.com/collections/willsinsta'
-
+//assigned variables from html~~~~~~~~~~~
 var pic = $('.imgUrl');
 var caption = $('.imgCaption');
 var entries = [];
-
-function collectEntries(entryObject) {
-	entries.push(entryObject);
-   
+//push object into entries array~~~~~~~~~
+// function collectEntries(entryObject) {
+// 	entries.push(entryObject);
+// }
+//onPost functions~~~~~~~~~~~~~~~~~~~~~~~
+function onPostSuccess(data) {
+    console.log('I run good.')
 }
+function onPostError(err) {
+    console.log('I run bad.');
+}
+function onPostComplete() {
+    console.log('I run.');
+}
+//on event to send entry~~~~~~~~~~~~~~~
 $('form').submit(function(e) {
     e.preventDefault();
+    //build entry object
     var entry = {
         image: pic.val(),
         description: caption.val()
     }
-   collectEntries(entry);
-   
-
-entries.map(function(posting) {
+    console.log(entry);
+    //post settings
     $.ajax({
     	url: collectionUrl,
     	type: 'post',
-        data: posting,
+        data: entry,
     	dataType: 'json',
-    	success: function(data) {
-    		console.log(entries)
-            return entries.pop();
-    	},
-    	error: function(err) {
-    		console.log(err)
-    	},
-        complete: function() {
-            console.log('I run.')
-        }
+    	success: onPostSuccess,
+    	error: onPostError,
+        complete: onPostComplete
     });
-});
 });
 
 
