443ec4ed8342f9393188836e726cae87cbb225c1
Will Cook <wellsee83@gmail.com>
2016-03-27 12:50:55 -0500

    register works

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //main page~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-var collectionUrl = 'http://small-tiyfe.herokuapp.com/collections/willsinsta'
+var collectionUrl;
 //assigned variables from html~~~~~~~~~~~
 var pic = $('#imgUrl');
 var caption = $('#imgCaption');
@@ -28,78 +28,153 @@ $('#stop').click(function() {
     noUrl.html('');
     noCap.html('');
 });
-//on event to send entry~~~~~~~~~~~~~~~~~
-$('form').submit(function(e) {
-    e.preventDefault();
-    //build entry object
-    var entry;
-    if ((pic.val().indexOf('http://') === -1) && (pic.val().indexOf('https://') === -1)) {
-        noUrl.html('URL must begin with "http://" or "https://"');
-        return;
-    } else if ((pic.val().indexOf('jpg') === -1) && (pic.val().indexOf('png') === -1) && (pic.val().indexOf('gif') === -1)) {
-        noUrl.html('URL must end with ".png", ".jpg", or ".gif"');
-        return;
+//Login Operation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$('#register').click(function() {
+    if ($('#un').val()==='') {
+        alert('Please create a unique username.');
+    } else if ($('#pw').val().length < 7) {
+        alert('Password must have at least 7 characters.');
     } else {
-        noUrl.html('');
-    }
-    if (caption.val() === '') {
-        noCap.html('You must enter a caption.');
-        return;
-    } else {
-        noCap.html('');
-    }
-    entry = {
-        image: pic.val(),
-        description: caption.val()
-    }
-    $('#imgUrl').val('');
-    $('#imgCaption').val('');
-    console.log(entry);
-    //post settings
-    $.ajax({
-    	url: collectionUrl,
-    	type: 'post',
-        data: entry,
-    	dataType: 'json',
-    	success: onPostSuccess,
-    	error: onPostError,
-        complete: onPostComplete
-    });
-    $('form').slideUp();
-    //Clear out old array~~~~~~~~~~~~~~~
+        var userName = $('#un').val();
+        collectionUrl = ('http://small-tiyfe.herokuapp.com/collections/'+userName);
+        var getSettings = {
+            url: collectionUrl,
+            type: 'get',
+            dataType: 'json',
+            success: function(data) {
+                var container = $('#container');
+                console.log(data);
+                data.forEach(function (val) {
+                    var outerBox = $('<div class="outerBox"></div>');
+                    var imageBox = $('<img>', {'src': val.image});
+                    var descriptionBox = $('<div id="description"></div>').html(val.description);
+
+                    outerBox.append(imageBox);
+                    outerBox.append(descriptionBox);
+
+                    container.append(outerBox);
+                });
+
+            },
+            error: function(err) {
+                console.log("Get run bad.");
+            }
+        }
+    }   
     $('#container').html('');
-    //Display new array~~~~~~~~~~~~~~~~~
     $.ajax(getSettings);
+    $('form').submit(function(e) {
+        e.preventDefault();
+        //build entry object
+        var entry;
+        if ((pic.val().indexOf('http://') === -1) && (pic.val().indexOf('https://') === -1)) {
+            noUrl.html('URL must begin with "http://" or "https://"');
+            return;
+        } else if ((pic.val().indexOf('jpg') === -1) && (pic.val().indexOf('png') === -1) && (pic.val().indexOf('gif') === -1)) {
+            noUrl.html('URL must end with ".png", ".jpg", or ".gif"');
+            return;
+        } else {
+            noUrl.html('');
+        }
+        if (caption.val() === '') {
+            noCap.html('You must enter a caption.');
+            return;
+        } else {
+            noCap.html('');
+        }
+        entry = {
+            image: pic.val(),
+            description: caption.val()
+        }
+        $('#imgUrl').val('');
+        $('#imgCaption').val('');
+        console.log(entry);
+        //post settings
+        $.ajax({
+            url: collectionUrl,
+            type: 'post',
+            data: entry,
+            dataType: 'json',
+            success: onPostSuccess,
+            error: onPostError,
+            complete: onPostComplete
+        });
+        $('form').slideUp();
+        //Clear out old array~~~~~~~~~~~~~~~
+        $('#container').html('');
+        //Display new array~~~~~~~~~~~~~~~~~
+        $.ajax(getSettings);
+    });
 });
 
+//on event to send entry~~~~~~~~~~~~~~~~~
+// $('form').submit(function(e) {
+//     e.preventDefault();
+//     //build entry object
+//     var entry;
+//     if ((pic.val().indexOf('http://') === -1) && (pic.val().indexOf('https://') === -1)) {
+//         noUrl.html('URL must begin with "http://" or "https://"');
+//         return;
+//     } else if ((pic.val().indexOf('jpg') === -1) && (pic.val().indexOf('png') === -1) && (pic.val().indexOf('gif') === -1)) {
+//         noUrl.html('URL must end with ".png", ".jpg", or ".gif"');
+//         return;
+//     } else {
+//         noUrl.html('');
+//     }
+//     if (caption.val() === '') {
+//         noCap.html('You must enter a caption.');
+//         return;
+//     } else {
+//         noCap.html('');
+//     }
+//     entry = {
+//         image: pic.val(),
+//         description: caption.val()
+//     }
+//     $('#imgUrl').val('');
+//     $('#imgCaption').val('');
+//     console.log(entry);
+//     //post settings
+//     $.ajax({
+//     	url: collectionUrl,
+//     	type: 'post',
+//         data: entry,
+//     	dataType: 'json',
+//     	success: onPostSuccess,
+//     	error: onPostError,
+//         complete: onPostComplete
+//     });
+//     $('form').slideUp();
+//     //Clear out old array~~~~~~~~~~~~~~~
+//     $('#container').html('');
+//     //Display new array~~~~~~~~~~~~~~~~~
+//     $.ajax(getSettings);
+// });
+
 //get event to display entries~~~~~~~~~~
-var getSettings = {
-	url: collectionUrl,
-	type: 'get',
-	dataType: 'json',
-	success: function(data) {
-        var container = $('#container');
-		console.log(data);
-        data.forEach(function (val) {
-            var outerBox = $('<div class="outerBox"></div>');
-            var imageBox = $('<img>', {'src': val.image});
-            var descriptionBox = $('<div id="description"></div>').html(val.description);
+// var getSettings = {
+// 	url: collectionUrl,
+// 	type: 'get',
+// 	dataType: 'json',
+// 	success: function(data) {
+//         var container = $('#container');
+// 		console.log(data);
+//         data.forEach(function (val) {
+//             var outerBox = $('<div class="outerBox"></div>');
+//             var imageBox = $('<img>', {'src': val.image});
+//             var descriptionBox = $('<div id="description"></div>').html(val.description);
 
-            outerBox.append(imageBox);
-            outerBox.append(descriptionBox);
+//             outerBox.append(imageBox);
+//             outerBox.append(descriptionBox);
 
-            container.append(outerBox);
-        });
+//             container.append(outerBox);
+//         });
 
-	},
-	error: function(err) {
-		console.log("Get run bad.");
-	}
-}
-//Display entries on page load~~~~~~~~~~
-$.ajax(getSettings);
-
-
+// 	},
+// 	error: function(err) {
+// 		console.log("Get run bad.");
+// 	}
+// }
 
 
 
